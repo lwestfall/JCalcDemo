6a8a52b046496d618bb5f57c36131067847ec3a5
Luke Westfall <lwestfall1@gmail.com>
2022-03-12 20:13:38 -0500

    Add two numbers

--- src/JCalc.java
+++ src/JCalc.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class JCalc {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to our calculator!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double input1 = Double.parseDouble(scanner.nextLine());
+        System.out.print("Enter the second number: ");
+        double input2 = Double.parseDouble(scanner.nextLine());
+
+        double sum = input1 + input2;
+        System.out.println(input1 + " + " + input2 + " = " + sum);
     }
 }
